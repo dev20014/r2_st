a7c557224ac95ceabc51b9b42c7f82320da4ec2c
dmm <dmm>
2024-03-09 12:59:00 +0700

    get list docs

--- app/app_image/models.py
+++ app/app_image/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 from blog_person.r2_storage import PrivateMediaStorage
+import uuid
+import os
+import logging
 
+def path_and_rename(prefix, filename):
+    ext = filename.split('.')[-1]
+    filename = '{}.{}'.format(uuid.uuid4().hex, ext)
+    return os.path.join(prefix, filename)
+
+
+def get_path_for_my_model_file(instance, filename):
+    path = path_and_rename('mymodelfiles/', filename)
+    print(path)
+    logging.info("path")
+    logging.info(path)
+    return path 
 
 class Document(models.Model):
     uploaded_at = models.DateTimeField(auto_now_add=True)
@@ -9,5 +24,5 @@ class Document(models.Model):
 
 class PrivateDocument(models.Model):
     # uploaded_at = models.DateTimeField(auto_now_add=True)
-    upload = models.FileField(storage=PrivateMediaStorage())
+    upload = models.FileField(upload_to=get_path_for_my_model_file, storage=PrivateMediaStorage())
     # user = models.ForeignKey(User, related_name='documents')
--- app/app_image/views.py
+++ app/app_image/views.py
@@ -4,9 +4,9 @@ from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-
+from rest_framework.serializers import ModelSerializer
 from .models import Document, PrivateDocument
-
+from rest_framework.viewsets import ModelViewSet
 
 def hello_world(request):
     return render(request, 'hello_world.html', {})
@@ -20,3 +20,15 @@ class PrivateDocumentCreateView(CreateView):
     model = PrivateDocument
     fields = ['upload', ]
     success_url = reverse_lazy('profile')
+
+
+# serializers.py
+class PrivateDocumentSerializer(ModelSerializer):
+    class Meta:
+        model = PrivateDocument
+        fields = '__all__'
+
+# views.py
+class PrivateDocumentViewSet(ModelViewSet):
+    queryset = PrivateDocument.objects.all()
+    serializer_class = PrivateDocumentSerializer
